670d9cff76a2f244afdf18c840c891224b06ce07
patriarch11 <o5zaichenko@gmail.com>
2022-08-27 00:56:29 +0300

    init0

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import json
 from typing import Any
 from datetime import datetime
 import requests
@@ -16,18 +17,18 @@ class CoinlayerParser:
         self.api_access_key = api_access_key
         self.base_url = base_url
 
-    def get_keys_values(self, options) -> dict[Any, Any]:
+    def get_keys_values(self, options: str) -> dict[Any, Any]:
         response = self.get_response(options)
         return dict.copy(response.json())
 
-    def get_response(self, option):
+    def get_response(self, option: str) -> json:
         url = f'{self.base_url}{option}'
         return requests.get(url)
 
-    def get_live_data(self):
+    def get_live_data(self) -> dict:
         return self.get_keys_values(f'live?access_key={self.api_access_key}')
 
-    def get_historical_data(self, date):
+    def get_historical_data(self, date: str) -> dict | str:
         try:
             date = datetime.strftime(date, r'%y-%m-%d')
         except Exception as ex:
